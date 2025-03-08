ab45e8be3312de91eda43d474d17e4236421bbbb
GusevSlava <v.gusev@a1qa.com>
2025-03-08 22:50:42 +0300

    add user-data-dir

--- wdioChrome.conf.js
+++ wdioChrome.conf.js
@@ -1,8 +1,5 @@
 const configWDIO = require("./wdio.conf");
 const downDir = require("./config");
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
 
 exports.config = {
     capabilities: [{
@@ -11,13 +8,13 @@ exports.config = {
         'goog:chromeOptions': {
             'prefs': {
                 'safebrowsing.enabled': true,
-                // 'download.default_directory': downDir,
+                'download.default_directory': downDir,
             },
             args: [
                 '--headless',
                 '--safebrowsing-disable-download-protection',
                 '--incognito',
-                `--user-data-dir=${fs.mkdtempSync(path.join(os.tmpdir(), 'chrome-user-data-'))}`
+                `--user-data-dir=app/chrome-user-data`
             ],
         },
         acceptInsecureCerts: true
